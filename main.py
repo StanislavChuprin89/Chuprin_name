77de8f3cfdbb13e80b7796638ecd884181bea2d4
chupr <chuprin@dnt-lab.com>
2022-01-26 19:50:55 +0200

    Ball

---
+++ main.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+color("green")
+pensize(5)
+right(180)
+forward(10)
+circle(50, 180)
+forward(10)
+
+penup()
+forward(80)
+left(90)
+forward(100)
+pendown()
+
+left(90)
+backward(10)
+forward(10)
+circle(25, 180)
+
+left(180)
+penup()
+circle(25, 100)
+pendown()
+circle(25, 260)
+done()
